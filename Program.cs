78b40e2b94b3e950377bfd55d53ff522176af645
Demo-net <155291854+Demo-net@users.noreply.github.com>
2023-12-31 13:46:52 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("llaa");
+            Console.WriteLine("palaa");
+            Console.ReadLine();
+        }
+    }
+}
